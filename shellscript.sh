e2d800e70974a791c9ed84688e31d1ae38e3f391
Arif-Hayat <arif1989aarzoo@gmail.com>
2023-10-01 15:00:48 +0000

    This is my first commit from ohio

---
+++ shellscript.sh
@@ -0,0 +1,14 @@
+# if_else example
+
+#! /bin/bash
+read -p "Enter the username:" username
+read -p "Enter the password:" password
+if [[ $username#! /bin/bash
+read -p "Enter the username:" username
+read -p "Enter the password:" password
+if [[ $username == "arif" && $password == "arif1989reza" ]];
+then
+        echo "Login succesfull"
+else
+        echo "Try again"
+fi
